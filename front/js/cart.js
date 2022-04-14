11b06d3ca5b2acaf2eccfd319e23e99f2cfc0b15
Picaljean313 <nony.adrien@gmail.com>
2022-04-14 14:56:09 +0200

    commmit de fin des étapes et d'avant modifications de finalisation du code

--- front/js/cart.js
+++ front/js/cart.js
@@ -129,4 +129,115 @@ window.addEventListener("load", function() {
 });
 
 
+let validation = false;
+
+let nameAndCityMask = /[^A-Za-z\-'\s]/g;
+let addressMask = /[^A-Za-z0-9\-'\s]/g;
+let emailMask = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+console.log(validation);
+
+document.getElementById("firstName").addEventListener("input", function(){
+  if (nameAndCityMask.test(document.getElementById("firstName").value)){
+    document.getElementById("firstNameErrorMsg").innerHTML = "Veuillez renseigner votre prénom en n'utilisant que des lettres sans accents, l'espace blanc, le trait d'union ou l'apostrophe";
+    validation = false;
+  }
+  else {
+    document.getElementById("firstNameErrorMsg").innerHTML = "";
+    validation = true;
+  }
+  console.log(validation);
+})
+
+document.getElementById("lastName").addEventListener("input", function(){
+  if (nameAndCityMask.test(document.getElementById("lastName").value)){
+    document.getElementById("lastNameErrorMsg").innerHTML = "Veuillez renseigner votre nom en n'utilisant que des lettres sans accents, l'espace blanc, le trait d'union ou l'apostrophe";
+    validation = false;
+  }
+  else {
+    document.getElementById("lastNameErrorMsg").innerHTML = "";
+    validation = true;
+  }
+  console.log(validation);
+})
+
+document.getElementById("address").addEventListener("input", function(){
+  if (addressMask.test(document.getElementById("address").value)){
+    document.getElementById("addressErrorMsg").innerHTML = "Veuillez renseigner votre addresse en n'utilisant que des lettres sans accents, les chiffres, l'espace blanc, le trait d'union ou l'apostrophe";
+    validation = false;
+  }
+  else {
+    document.getElementById("addressErrorMsg").innerHTML = "";
+    validation = true;
+  }
+  console.log(validation);
+})
+
+document.getElementById("city").addEventListener("input", function(){
+  if (nameAndCityMask.test(document.getElementById("city").value)){
+    document.getElementById("cityErrorMsg").innerHTML = "Veuillez renseigner votre ville en n'utilisant que des lettres sans accents, l'espace blanc, le trait d'union ou l'apostrophe";
+    validation = false;
+  }
+  else {
+    document.getElementById("cityErrorMsg").innerHTML = "";
+    validation = true;
+  }
+  console.log(validation);
+})
+
+document.getElementById("email").addEventListener("input", function(){
+  if (emailMask.test(document.getElementById("email").value)){
+    document.getElementById("emailErrorMsg").innerHTML = "";
+    validation = true;
+  }
+  else {
+    document.getElementById("emailErrorMsg").innerHTML = "Veuillez renseigner votre mail correctement";
+    validation = false;
+  }
+  console.log(validation);
+})
+
+document.getElementById("order").addEventListener("click", function(event){
+  if (!validation) {
+    event.preventDefault();
+  }
+  else {
+    event.preventDefault();
+    let contactOrder = {
+      firstName : document.getElementById("firstName").value,
+      lastName : document.getElementById("lastName").value,
+      address : document.getElementById("address").value,
+      city : document.getElementById("city").value,
+      email : document.getElementById("email").value
+    };
+    console.log(contactOrder);
+
+    let productsOrder = [];
+
+    for (let i in cart){
+      productsOrder.push(cart[i].id);
+    }
+
+
+
+    fetch("http://localhost:3000/api/products/order", {
+      method: "POST",
+      headers: { 
+        'Accept': 'application/json', 
+        'Content-Type': 'application/json' 
+        },
+      body: JSON.stringify({contact : contactOrder, products : productsOrder})
+    })
+    .then(function(res){
+      if (res.ok){
+          return res.json()
+      }
+    })
+    .then(function(value){
+      console.log(value);
+      document.location.href=`../html/confirmation.html?orderId=${value.orderId}`;
+    });
+
+
+  }
+})
 
